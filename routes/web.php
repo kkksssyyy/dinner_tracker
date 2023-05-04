ea9c8db76c74c828e017646ded78fa16cc05cd63
Kyohei Sato <kyoheisato0410@gmail.com>
2023-05-04 19:54:14 +0900

    ルーティングの設定とコントローラを追加

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,27 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/search', [SearchController::class, 'search']);
+Route::get('/user', [UserController::class, 'show']);
+// Route::get('/login', [AuthController::class, 'showLoginForm']);
+// Route::post('/login', [AuthController::class, 'login']);
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::post('/posts', [PostController::class, 'store']);
+Route::get('/posts/{post_id}', [PostController::class, 'show']);
+Route::get('/posts/{post_id}/edit', [PostController::class, 'edit']);
+Route::put('/posts/{post_id}', [PostController::class, 'update']);
+Route::delete('/posts/{post_id}', [PostController::class, 'destroy']);
+
+Route::get('/groups', [GroupController::class, 'index']);
+Route::get('/groups/create', [GroupController::class, 'create']);
+Route::post('/groups', [GroupController::class, 'store']);
+Route::get('/groups/{group_id}', [GroupController::class, 'show']);
+Route::get('/groups/{group_id}/edit', [GroupController::class, 'edit']);
+Route::put('/groups/{group_id}', [GroupController::class, 'update']);
+Route::delete('/groups/{group_id}', [GroupController::class, 'destroy']);
+
+Route::get('/mypage', [UserController::class, 'mypage']);
+
 require __DIR__.'/auth.php';
