77fb1dc937fd853d34efed42023c565a47e5708b
Kyohei Sato <kyoheisato0410@gmail.com>
2023-05-05 17:08:32 +0900

    デフォルトで設定されていたwelcomeページへのルーティングをコメントアウト

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +19,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -29,7 +34,7 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::get('/search', [SearchController::class, 'search']);
-Route::get('/user', [UserController::class, 'show']);
+Route::get('/user/{user_id}', [UserController::class, 'show']);
 // Route::get('/login', [AuthController::class, 'showLoginForm']);
 // Route::post('/login', [AuthController::class, 'login']);
 Route::get('/', [HomeController::class, 'index']);
